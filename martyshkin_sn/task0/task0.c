143c467664ef1ca86de28d08e01ab6153e1a79df
Zinderlan <stas.martyshkin.08@mail.ru>
2025-09-29 10:23:04 +0300

    Заменил все русские буквы на английские

--- martyshkin_sn/task0/task0.c
+++ martyshkin_sn/task0/task0.c
@@ -10,24 +10,24 @@ void main()
     double center2_x, center2_y;
     double rast;
 
-    printf("Введите координаты первой окружности и её радиус: \n");
+    printf("Enter the coordinates of the first circle and its radius: \n");
     scanf_s("%lf %lf %lf", &center1_x, &center1_y, &r1);
-    printf("Ввдите координаты второй окружности и её радиус: \n");
+    printf("Enter the coordinates of the second circle and its radius: \n");
     scanf_s("%lf %lf %lf", &center2_x, &center2_y, &r2);
     printf("%lf %lf %lf \n", center1_x, center1_y, r1);
     printf("%lf %lf %lf \n", center2_x, center2_y, r2);
     rast = pow(pow((center1_x - center2_x), 2) + pow((center1_y - center2_y), 2), 0.5);
     if (rast == r1 + r2) {
-        printf("1. Окружности касаются");
+        printf("1. Circles touch");
     }
     else if ((center1_x == center2_x) && (center1_y == center2_y)) {
-        if (r1 == r2) { printf("2.1 Окружности идиентичны"); }
-        else { printf("2.2 Одна окружность находится в другой"); }
+        if (r1 == r2) { printf("2.1 The circles are identical \n"); }
+        else { printf("2.2 One circle is located inside another \n"); }
     }
     else if (rast < r1 + r2) {
-        printf("3. Окружности пересекаются");
+        printf("3. The circles intersect \n");
     }
     else if (rast > r1 + r2) {
-        printf("4. Окружности не пересекаются");
+        printf("4. The circles do not intersect \n");
     }
 }
