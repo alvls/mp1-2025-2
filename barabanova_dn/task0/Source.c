6e78cd9027d694856ec28dad22a599ebb765d5c3
дашенька <dollince@yandex.ru>
2025-10-05 22:30:31 +0300

    Перевела в task 0 русские слова на английский

--- barabanova_dn/task0/Source.c
+++ barabanova_dn/task0/Source.c
@@ -5,38 +5,39 @@ int main()
 {
     double x1, y1, r1, x2, y2, r2;
 
-    printf("Введите координаты центра и радиуса первой окружности без запятой x1 y1 r1: ");
+    printf("Enter the coordinates of the center and radius of the first circle without a comma x1 y1 r1: ");
     scanf_s("%lf %lf %lf", &x1, &y1, &r1);
 
-    printf("Введите координаты центра и радиуса второй окружности без запятой x2 y2 r2: ");
+    printf("Enter the coordinates of the center and radius of the second circle without a comma x2 y2 r2: ");
     scanf_s("%lf %lf %lf", &x2, &y2, &r2);
 
     double d = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
     if (d == 0 && r1 == r2)
     {
-        printf("Окружности совпадают.\n");
+        printf("The circles coincide.\n");
     }
     else if (d > r1 + r2)
     {
-        printf("Окружности не пересекаются (внешние).\n");
+        printf("The outer circles do not intersect.\n");
     }
     else if (d == r1 + r2)
     {
-        printf("Окружности касаются внешним образом.\n");
+        printf("The circles touch externally.\n");
     }
     else if (d < fabs(r1 - r2))
     {
-        printf("Одна окружность внутри другой целиком.\n");
+        printf("One circle inside another entirely.\n");
     }
     else if (d == fabs(r1 - r2))
     {
-        printf("Окружности касаются внутренним образом.\n");
+        printf("The circles touch internally.\n");
     }
     else
     {
-        printf("Окружности пересекаются в двух точках.\n");
+        printf("The circles intersect at two points.\n");
     }
-  
+    getchar();
+    getchar();
     return 0;
 }
