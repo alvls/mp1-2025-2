20f6c1f4dcf9831ad9ae458047e3ce1f199b42e0
Grenvime <lyutiiandryusha@gmail.com>
2025-10-18 15:43:25 +0300

    ignore

---
+++ Kurapin_AA/task0/Source.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+	double x1, y1, R1, x2, y2, R2;
+
+	printf("Type x1, y1:\n");
+	scanf_s("%lf %lf", &x1, &y1);
+	printf("Print R1:\n");
+	scanf_s("%lf", &R1);
+	printf("Type x2, y2:\n");
+	scanf_s("%lf %lf", &x2, &y2);
+	printf("Print R2:\n");
+	scanf_s("%lf", &R2);
+
+	return 1;
+}
