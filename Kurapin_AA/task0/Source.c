78d5a2ab8b6178c592b72dcb4349650b6771758c
Grenvime <lyutiiandryusha@gmail.com>
2025-10-19 16:18:09 +0300

    тест

--- Kurapin_AA/task0/Source.c
+++ Kurapin_AA/task0/Source.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
+п»ї#include <stdio.h>
 #include <math.h>
 #include <locale.h>
 
 int main()
 {
-	system("chcp 1251");
 	setlocale(LC_ALL, "Rus");
 
 	double x1, y1, R1, x2, y2, R2;
 
-	printf("Введите x1, y1:\n");
+	printf("Р’РІРµРґРёС‚Рµ x1, y1:\n");
 	scanf_s("%lf %lf", &x1, &y1);
-	printf("Введите R1:\n");
+	printf("Р’РІРµРґРёС‚Рµ R1:\n");
 	scanf_s("%lf", &R1);
-	printf("Введите x2, y2:\n");
+	printf("Р’РІРµРґРёС‚Рµ x2, y2:\n");
 	scanf_s("%lf %lf", &x2, &y2);
-	printf("Введите R2:\n");
+	printf("Р’РІРµРґРёС‚Рµ R2:\n");
 	scanf_s("%lf", &R2);
 
 	double distance = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
@@ -27,22 +26,22 @@ int main()
 		maxC = 2, minC = 1;
 
 	if (distance > R1 + R2) {
-		printf("Окружности не пересекаются\n");
+		printf("РћРєСЂСѓР¶РЅРѕСЃС‚Рё РЅРµ РїРµСЂРµСЃРµРєР°СЋС‚СЃСЏ\n");
 	}
 	else if (distance < abs(R1 - R2)) {
-		printf("Окружность %i полностью находится внутри окружности %i, они не касаются\n", minC, maxC);
+		printf("РћРєСЂСѓР¶РЅРѕСЃС‚СЊ %i РїРѕР»РЅРѕСЃС‚СЊСЋ РЅР°С…РѕРґРёС‚СЃСЏ РІРЅСѓС‚СЂРё РѕРєСЂСѓР¶РЅРѕСЃС‚Рё %i, РѕРЅРё РЅРµ РєР°СЃР°СЋС‚СЃСЏ\n", minC, maxC);
 	}
 	else if (distance = R1 + R2) {
-		printf("Окружности касаются внешне\n");
+		printf("РћРєСЂСѓР¶РЅРѕСЃС‚Рё РєР°СЃР°СЋС‚СЃСЏ РІРЅРµС€РЅРµ\n");
 	}
 	else if (distance == abs(R1 - R2) && distance != 0) {
-		printf("Окружность %i находится внутри окружности %i, они касаются внутренне\n", minC, maxC);
+		printf("РћРєСЂСѓР¶РЅРѕСЃС‚СЊ %i РЅР°С…РѕРґРёС‚СЃСЏ РІРЅСѓС‚СЂРё РѕРєСЂСѓР¶РЅРѕСЃС‚Рё %i, РѕРЅРё РєР°СЃР°СЋС‚СЃСЏ РІРЅСѓС‚СЂРµРЅРЅРµ\n", minC, maxC);
 	}
 	else if (abs(R1 - R2) < distance < R1 + R2) {
-		printf("Окружности пересекаются в двух точках\n");
+		printf("РћРєСЂСѓР¶РЅРѕСЃС‚Рё РїРµСЂРµСЃРµРєР°СЋС‚СЃСЏ РІ РґРІСѓС… С‚РѕС‡РєР°С…\n");
 	}
 	else if (distance == 0 && R1 == R2) {
-		printf("Окружности совпадают\n");
+		printf("РћРєСЂСѓР¶РЅРѕСЃС‚Рё СЃРѕРІРїР°РґР°СЋС‚\n");
 	}
 
 	system("pause");
