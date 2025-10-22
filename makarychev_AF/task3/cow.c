2eedb23aad2bea34b62249ed48efd4b3e28db69d
Alex34083 <alexei3408@gmail.com>
2025-10-22 21:22:02 +0300

    Исправление задачи 3 попытка 2

--- makarychev_AF/task3/cow.c
+++ makarychev_AF/task3/cow.c
@@ -6,16 +6,18 @@ void main()
 {
 	char* locale = setlocale(LC_ALL, "");
 	srand(time(NULL));
-	int  num[5], num_choise[11] = {0,1,2,3,4,5,6,7,8,9},num_guess[5];
-	int len, i, a = rand() % 10, flag=1,cow,bull,j;
+	int  num[5], num_choise[11] = { 0,1,2,3,4,5,6,7,8,9 }, num_guess[5];
+	int len, i, a = rand() % 10, flag = 1, cow, bull, j;
 	printf("Введите длину числа: ");
 	scanf_s("%d", &len);
 	for (i=0; i <= len - 1; i++)
 	{
 		while ((num[i]>10)||(num[i]<0))
 		{
-			if (i==0) a=1+ rand() % 10;
-			else a = rand() % 10;
+			if 
+				(i==0) a=1+ rand() % 10;
+			else 
+				a = rand() % 10;
 			num[i] = num_choise[a];
 			num_choise[a] = -1;
 		}	
@@ -28,8 +30,10 @@ void main()
 		{
 			printf("Какая цифра стоит на позиции %d: ", i+1);
 			scanf_s("%d", &num_guess[i]);
-			if (num_guess[i] == num[i]) bull++;
-			else for (j = 0; j <= len - 1; j++) if (num_guess[i] == num[j]) cow++;
+			if 
+				(num_guess[i] == num[i]) bull++;
+			else 
+				for (j = 0; j <= len - 1; j++) if (num_guess[i] == num[j]) cow++;
 		}
 		if (bull == len)
 		{ 
@@ -37,6 +41,7 @@ void main()
 			for (j = 0; j <= len - 1; j++) printf("%d", num[j]);
 			flag = 0;
 		}
-		else printf("Количество коров: %d\nКоличество быков: %d\n", cow, bull);
+		else 
+			printf("Количество коров: %d\nКоличество быков: %d\n", cow, bull);
 	}	
 }
