f5d3dcaad4fdf5f6309e3e81fe0b3bea66c9d418
Bukharov Dmitry <bukharovdmiry73@gmail.com>
2025-10-26 17:54:18 +0300

    Правки по заданию 1, сделал 2 и 3 задания

--- Bukharov_DA/task1/Source.c
+++ Bukharov_DA/task1/Source.c
@@ -4,11 +4,23 @@
 int main() {
 	setlocale(LC_ALL, "Russian");
 
-	float pDVP = 0.85f, pDSP = 0.7f, pWood = 0.55f, mass_of_back_wall, mass_of_walls, mass_of_covers, mass_of_doors, mass_of_shelves, mass; // плотности в г/см^3 
+	float pDVP = 0.85f,   // плотности в г/см^3 
+		pDSP = 0.7f,
+		pWood = 0.55f;
+
+	float mass_of_back_wall, mass_of_walls, mass_of_covers,
+		mass_of_doors, mass_of_shelves, mass; 
+
 	int h, w, d, n;
 
 	printf("Введите высоту шкафа в диапазоне от 180 до 220 см, ширину в диапазоне от 80 до 120 см, глубину в диапазоне от 50 до 90 см\n");
 	scanf_s("%d %d %d", &h, &w, &d);
+	
+	while (h > 220 || h < 180 || w < 80 || w > 120 || d < 50 || d > 90) {
+		printf("Вы неправильно ввели размеры шкафа. Попробуйте еще раз\n");
+		scanf_s("%d %d %d", &h, &w, &d);
+	}
+	
 
 	n = h / 40;
 	mass_of_back_wall = h * w * 0.5 * pDVP;
