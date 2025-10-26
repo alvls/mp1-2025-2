63b80f455e85a783dd056888ca65f1a5ca085485
Bukharov Dmitry <bukharovdmiry73@gmail.com>
2025-10-26 19:54:37 +0300

    Правки по заданию 1, сделал 2 и 3 задания

---
+++ Bukharov_DA/task2/Source.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	srand(time(0));
+	int gamemode, rnum, guess, number;
+	char continue_playing = 'y';
+
+	while (continue_playing == 'y' || continue_playing == 'Y') {
+		int answer = 0, attempts = 1, min = 1, max = 1000;
+		printf("Выберите режим игры:\n");
+		printf("1 - Вы угадываете число\n");
+		printf("2 - Компьютер угадывает число\n");
+		printf("Ваш выбор: ");
+		scanf_s("%d", &gamemode);
+		if (gamemode == 1) {
+			printf("Режим 1\n");
+			rnum = rand() % 999 + 1;
+			while (answer == 0) {
+				printf("Попробуйте угадать число, которое я загадал:\n");
+				scanf_s("%d", &guess);
+				if (guess == rnum) {
+					printf("Вы угадали!\nКоличество попыток: %i\n", attempts);
+					answer = 1;
+				}
+				else if (guess < rnum) {
+					printf("Загаданное число больше\n");
+					attempts += 1;
+				}
+				else if (guess > rnum) {
+					printf("Загаданное число меньше\n");
+					attempts += 1;
+				}
+			}
+		}
+		else if (gamemode == 2) {
+			printf("Режим 2\n");
+			printf("Загадайте число в диапазоне от 1 до 1000 и введите его: ");
+			//printf("????????? ?????????? ???????? ??? ?????, ??????? ????????? ?????????.???? ?????????? ????? ??????, ??????? '>',\n???? ?????????? ????? ??????, ??????? '<', ???? ????????? ???????? ???? ?????, ??????? '='\n");
+			scanf_s("%d", &number);
+
+			while (number > 1000 || number < 1) {
+				printf("Вы ввели число не подходящее диапазону. Введите число от 1 до 1000: ");
+				scanf_s("%d", &number);
+			}
+
+			char comparison = '.';
+			attempts = 1;
+			answer = 0;
+
+			while (answer == 0) {
+				guess = (min + max) / 2;
+				printf("Программа предполагает, что ваше число: %d\n", guess);
+				printf("Введите '>', '<' или '='\n");
+				scanf_s(" %c", &comparison, 1);
+
+				if (comparison == '=') {
+					printf("Число угадано\n");
+					printf("Количество попыток: %d\n", attempts);
+					answer = 1;
+				}
+				else if (comparison == '>') {
+					min = guess;
+					attempts += 1;
+				}
+				else if (comparison == '<') {
+					max = guess;
+					attempts += 1;
+				}
+				else {
+					printf("Неверный ввод, используйте только '>', '<' или '='\n");
+				}
+			}
+
+		}
+		else {
+			printf("Неверный выбор!\n");
+		}
+		printf("\nХотите сыграть еще раз? (y/n): ");
+		scanf_s(" %c", &continue_playing, 1);
+	}
+
+	system("pause");
+}
