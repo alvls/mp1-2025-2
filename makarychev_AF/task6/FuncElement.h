2188914337dc89e5d5dfad8a0b4003f55d766376
Alex34083 <alexei3408@gmail.com>
2025-12-21 02:06:58 +0300

    Задание 6

---
+++ makarychev_AF/task6/FuncElement.h
@@ -0,0 +1,19 @@
+#ifndef FUNC_ELEMENT_H
+#define FUNC_ELEMENT_H
+
+
+	double Exp(double y_past, double y, int n);
+	double Sin(double y_past, double y, int n);
+	double Cos(double y_past, double y, int n);
+	double Cosh(double y_past, double y, int n);
+
+	double Arcsin(double y_past, double y, int n);
+	double LnX(double y_past, double y, int n);
+	double Ln1X(double y_past, double y, int n);
+	double sqrtX(double y_past, double y, int n);
+	double Arctg(double y_past, double y, int n);
+
+	double Sinh(double y_past, double y, int n);
+	double Arcsinh(double y_past, double y, int n);
+
+#endif
